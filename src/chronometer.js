c1f05654183fbc7dfaf385e01f7f922227f62e1e
Sophia Ziesch <s.ziesch@posteo.de>
2023-06-20 17:18:04 +0200

    basically done, only stop not complete

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    setInterval(() => {
+      this.currentTime += 1;
+        if (printTimeCallback) {
+        printTimeCallback();
+        }
+      }
+    , 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const totalMin = Math.floor(this.currentTime/60)
+    return  totalMin;
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigNum = "";
+    if(value < 10){
+      twoDigNum = "0"+value.toString();
+    }else {
+      twoDigNum = value.toString();
+    }
+    return twoDigNum;
+    /* return (value).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) */
+
+/*     if (this.getMinutes.length < 2) {
+      return `0${this.getMinutes.value}`
+    } else {
+      return `${this.getMinutes.value}`
+    }
+ */
+    
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const min = this.computeTwoDigitNumber(this.getMinutes())
+    const sec = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${min}:${sec}`
   }
 }
